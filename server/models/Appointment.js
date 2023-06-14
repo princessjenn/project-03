7a1df431fd56bd0f2e8651a9d759e003dd7dcdac
unknown <thomasjjohnson9311@gmail.com>
2023-06-14 10:12:44 -0500

    test

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,53 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const appointmentSchema = new Schema({
-    id: {
-        type: Int,
-        required: true,
-    },
-    userId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    barberId: {
-        type: INT,
-        required: true,
-        unique: true,
-    },
-    barberName: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    specialty: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-    },
-    time: {
-        type: TimeRanges,
-        required: true,
-        unique: true,
-    },
-
-
+  id: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  barberId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  barberName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  specialty: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
 });
 
-
-
-
-
-
-
-const Appointment = model('Appointment', appointmentSchema);
+const Appointment = model("Appointment", appointmentSchema);
 
 module.exports = Appointment;
