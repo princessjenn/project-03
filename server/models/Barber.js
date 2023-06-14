29eeb8f25f72c67c16d590916591a3c728f1532b
benjiwildcat <benjiwildcat@gmail.com>
2023-06-13 23:12:58 -0500

    finished barber-appointment-specialty models

--- server/models/Barber.js
+++ server/models/Barber.js
@@ -2,6 +2,24 @@ const { Schema, model } = require('mongoose');
 
 const barberSchema = new Schema({
 
+    userId: {
+        type: INT,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+
+    specialties: [String],
+
+    availability: [{
+        day: String,
+        startTime: String,
+        endTime: String,
+    }],
+
 });
 
 
