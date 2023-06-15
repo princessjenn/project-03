e97515e34acc3f0f807a8cd02311024088951f2e
Jennifer Eckenrode <j.eckenrode@me.com>
2023-06-15 11:18:21 -0500

    installed tailwind

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../../utils/queries';
 
-import Auth from '../utils/auth';
+import Auth from '../../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
