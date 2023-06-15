0dc1b867174d3d83eff5aed00d0c4641753418b9
Jennifer Eckenrode <j.eckenrode@me.com>
2023-06-15 17:10:57 -0500

    implemented daisyUI, and gave signup page the correct fields and isAdmin field

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["night"],
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
