10b47aebe3f8cfedaeeca0da043c3cc71d18af78
Jennifer Eckenrode <j.eckenrode@me.com>
2023-06-18 16:17:07 -0500

    created a design template for Barbers Page, and also added a deconstructed coffee theme in the tailwind.config.js file so that we can easily grab the colors and implement customization in the theme

--- client/src/pages/Barbers/index.jsx
+++ client/src/pages/Barbers/index.jsx
@@ -3,22 +3,62 @@ import React from "react";
 export default function Barbers() {
   return (
     <div>
-      <h1>Barbers Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <div className="flex flex-col w-full lg:flex-row">
+        <div className="flex-grow">
+          <div className="card w-96 glass">
+            <figure>
+              <img src="/images/anthony-banks.jpg" alt="Anthony Banks" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-xl text-error font-bold">Anthony Banks</h2>
+              <p className="text-sm text-primary font-bold">
+                Anthony Banks, the co-owner of The Fade Brigade Barber Shop, is a
+                true master of his craft. Anthony Banks, the co-owner of The Fade
+                Brigade Barber Shop, is a true master of his craft. With over 15
+                years of experience in the industry, Anthony's passion for
+                barbering runs deep. Known for his meticulous attention to detail
+                and precision, he has earned a loyal following of clients who
+                trust him with their hair. Anthony's friendly and approachable
+                demeanor creates a welcoming atmosphere in the shop, where clients
+                can sit back, relax, and enjoy a top-notch grooming experience.
+                Whether you're looking for a classic clean cut or a modern style,
+                Anthony's expert hands will leave you feeling confident and
+                looking sharp.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="divider lg:divider-horizontal"></div>
+        <div className="flex-grow">
+          <div className="card w-96 glass">
+            <figure>
+              <img src="/images/david-smith.jpg" alt="David Smith" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-xl text-error font-bold">David Smith</h2>
+              <p className="text-sm text-primary font-bold">
+                Meet David Smith, the other half of The Fade Brigade Barber Shop's
+                dynamic duo. As a co-owner and skilled barber, David brings a
+                unique blend of creativity and expertise to the barbershop. With a
+                keen eye for trends and a passion for experimenting with different
+                styles, David loves helping clients find their signature look.
+                From edgy fades to suave beard trims, his artistic touch and
+                attention to individuality make every visit to his chair an
+                opportunity for self-expression. David's infectious energy and
+                warm personality create an environment where clients can feel
+                comfortable and inspired to try something new. Step into his chair
+                and let David work his magic, leaving you with a hairstyle that
+                reflects your personality and boosts your confidence.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto mt-8">
+        <div className="flex justify-center">
+          <button className="btn btn-primary">Book Now!</button>
+        </div>
+      </div>
     </div>
   );
 }
