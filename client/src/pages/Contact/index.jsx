afd456fa72d40e1991d9df3136e9bab016c3db5a
Jennifer Eckenrode <j.eckenrode@me.com>
2023-06-18 20:37:14 -0500

    started the contact page with the barbershop's contact info, will be adding contact form afterwards

--- client/src/pages/Contact/index.jsx
+++ client/src/pages/Contact/index.jsx
@@ -2,21 +2,40 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+<div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">The Fade Brigade Barbershop</h1>
+      <p className="py-6">ADDRESS: 42 Wallaby Way, Suite 100, Dallas, TX 12345</p>
+      <p className="py-6">PHONE NUMBER: (555) 123-CUTZ (2889)</p>
+      <p className="py-6">EMAIL ADDRESS: grooming@TheFadeBrigade.com</p>
+      <p className="py-6">HOURS:</p>
+      <p className="py-2">MONDAY | 9AM - 9AMPM</p>
+      <p className="py-2">TUESDAY | 1PM - 9PM</p>
+      <p className="py-2">WEDNESDAY | 9AM - 9PM</p>
+      <p className="py-2">THURSDAY | 9AM - 9PM</p>
+      <p className="py-2">FRIDAY | 9AM - 9PM</p>
+      <p className="py-2">SATURDAY | 9AM - 6PM</p>
+      <p className="py-2">SUNDAY | CLOSED</p>
+      <div className="flex justify-center py-6">
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="mr-2">
+          <i className="fab fa-facebook fa-2x"></i>
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="mr-2">
+          <i className="fab fa-twitter fa-2x"></i>
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="mr-2">
+          <i className="fab fa-instagram fa-2x"></i>
+        </a>
+        <a href="https://plus.google.com" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-google-plus fa-2x"></i>
+        </a>
+      </div>
+      <button className="btn btn-primary">Get Started</button>
     </div>
+  </div>
+</div>
   );
 }
+
+  {/* <figure><iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d214191.11667407426!2d-96.90958709272088!3d32.98436104662141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d33.131371!2d-96.8089857!4m5!1s0x864e9f0447c04e15%3A0xc05fcab81b99542e!2sSouthern%20Methodist%20University%2C%20Dallas%2C%20TX!3m2!1d32.841217799999995!2d-96.78451749999999!5e0!3m2!1sen!2sus!4v1687137209764!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></figure> */}
