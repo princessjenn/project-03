d8ee6a18a211740ed47a2a07a4a0bb102dd93203
Jennifer Eckenrode <j.eckenrode@me.com>
2023-06-20 11:06:47 -0500

    profile page logged in message

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -36,8 +36,7 @@ const Profile = () => {
     <div>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : "your"} profile.
-        </h2>
+          You are Logged in. </h2>
 
         {/* <div className="col-12 col-md-10 mb-5"> */}
         {/* <ThoughtList */}
