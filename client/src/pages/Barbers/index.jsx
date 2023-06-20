5613b294f1f37c6e45da7e046e84c1a9a482d619
unknown <thomasjjohnson9311@gmail.com>
2023-06-19 21:06:20 -0500

    ig and tw icons for David Smith

--- client/src/pages/Barbers/index.jsx
+++ client/src/pages/Barbers/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
 
 export default function Barbers() {
   return (
@@ -7,14 +9,31 @@ export default function Barbers() {
         <div className="flex-grow">
           <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-              <img src="../../../../public/assets/pexels-janik-butz-2776124.jpg" alt="Anthony Banks" className="rounded-xl" />
+              <img
+                src="../../../../public/assets/pexels-janik-butz-2776124.jpg"
+                alt="Anthony Banks"
+                className="rounded-xl"
+              />
             </figure>
             <div className="card-body items-center text-center">
-              <h2 className="card-title text-xl text-error font-bold">Anthony Banks</h2>
+              <h2 className="card-title text-xl text-error font-bold">
+                Anthony Banks
+              </h2>
               <p className="text-sm text-primary">
-                Anthony Banks, the co-owner of The Fade Brigade Barber Shop, is a true master of his craft.
-                <br /><br />
-                Anthony Banks, the co-owner of The Fade Brigade Barber Shop, is a true master of his craft. With over 15 years of experience in the industry, Anthony's passion for barbering runs deep. Known for his meticulous attention to detail and precision, he has earned a loyal following of clients who trust him with their hair. Anthony's friendly and approachable demeanor creates a welcoming atmosphere in the shop, where clients can sit back, relax, and enjoy a top-notch grooming experience. Whether you're looking for a classic clean cut or a modern style, Anthony's expert hands will leave you feeling confident and looking sharp.
+                Anthony Banks, the co-owner of The Fade Brigade Barber Shop, is
+                a true master of his craft.
+                <br />
+                <br />
+                Anthony Banks, the co-owner of The Fade Brigade Barber Shop, is
+                a true master of his craft. With over 15 years of experience in
+                the industry, Anthony's passion for barbering runs deep. Known
+                for his meticulous attention to detail and precision, he has
+                earned a loyal following of clients who trust him with their
+                hair. Anthony's friendly and approachable demeanor creates a
+                welcoming atmosphere in the shop, where clients can sit back,
+                relax, and enjoy a top-notch grooming experience. Whether you're
+                looking for a classic clean cut or a modern style, Anthony's
+                expert hands will leave you feeling confident and looking sharp.
               </p>
             </div>
           </div>
@@ -23,15 +42,42 @@ export default function Barbers() {
         <div className="flex-grow">
           <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-              <img src="../../../../public/assets/pexels-janik-butz-2776124.jpg" alt="David Smith" className="rounded-xl" />
+              <img
+                src="../../../../public/assets/pexels-janik-butz-2776124.jpg"
+                alt="David Smith"
+                className="rounded-xl"
+              />
             </figure>
             <div className="card-body items-center text-center">
-              <h2 className="card-title text-xl text-error font-bold">David Smith</h2>
+              <h2 className="card-title text-xl text-error font-bold">
+                David Smith
+              </h2>
               <p className="text-sm text-primary">
-                Meet David Smith, the other half of The Fade Brigade Barber Shop's dynamic duo.
-                <br /><br />
-                As a co-owner and skilled barber, David brings a unique blend of creativity and expertise to the barbershop. With a keen eye for trends and a passion for experimenting with different styles, David loves helping clients find their signature look. From edgy fades to suave beard trims, his artistic touch and attention to individuality make every visit to his chair an opportunity for self-expression. David's infectious energy and warm personality create an environment where clients can feel comfortable and inspired to try something new. Step into his chair and let David work his magic, leaving you with a hairstyle that reflects your personality and boosts your confidence.
+                Meet David Smith, the other half of The Fade Brigade Barber
+                Shop's dynamic duo.
+                <br />
+                <br />
+                As a co-owner and skilled barber, David brings a unique blend of
+                creativity and expertise to the barbershop. With a keen eye for
+                trends and a passion for experimenting with different styles,
+                David loves helping clients find their signature look. From edgy
+                fades to suave beard trims, his artistic touch and attention to
+                individuality make every visit to his chair an opportunity for
+                self-expression. David's infectious energy and warm personality
+                create an environment where clients can feel comfortable and
+                inspired to try something new. Step into his chair and let David
+                work his magic, leaving you with a hairstyle that reflects your
+                personality and boosts your confidence.
               </p>
+              <div className="flex">
+                <span className="px-5">
+                  <FaInstagram size="25px"></FaInstagram>
+                </span>
+                <span>
+                  {" "}
+                  <BsTwitter size="25px"></BsTwitter>
+                </span>
+              </div>
             </div>
           </div>
         </div>
